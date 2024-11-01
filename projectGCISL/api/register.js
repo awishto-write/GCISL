493d301a53082fd475f434b674b1ae86efea1364
Naomi Dion-Gokan <122447778+awishto-write@users.noreply.github.com>
2024-10-31 20:57:33 -0700

    Update register.js

--- projectGCISL/api/register.js
+++ projectGCISL/api/register.js
@@ -1,11 +1,7 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-
-//app.get('/api', (req, res) => {
-app.post('/register', (req, res) => {
- res.status(200).json({ message: "Index route is working!" });
-});
-
-module.exports = app;
-module.exports.handler = serverless(app);
+module.exports = (req, res) => {
+  if (req.method === 'POST') {
+    res.status(200).json({ message: "User registered successfully!" });
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+};
